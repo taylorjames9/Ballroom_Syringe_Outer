870a64238602525c972bd70905ec48656f1dd592
James Taylor <taylorjames9@gmail.com>
2014-05-14 14:14:05 -0500

    Before Suitor starts moving

---
+++ Syringe_Ballroom/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+
+		public GameObject main_cameraObject; 
+		public GameObject second_cameraObject; 
+
+		void OnCollisionEnter2D(Collision2D other){
+				main_cameraObject.camera.enabled = false;
+				second_cameraObject.camera.enabled = true;
+		}
+}
