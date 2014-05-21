b7a96d1265cb711f6b21d7283102c00d85c11dc0
James Taylor <taylorjames9@gmail.com>
2014-05-21 10:33:48 -0500

    walkk left and walk right animations now working correctly

--- Syringe_Ballroom/Assets/Scripts/MainCharacterScript.cs
+++ Syringe_Ballroom/Assets/Scripts/MainCharacterScript.cs
@@ -33,78 +33,62 @@ public class MainCharacterScript : MonoBehaviour {
 
 		IEnumerator Movement (Vector3 target)
 		{
-				while(Vector3.Distance(transform.position, target) > 0.05f)
+
+				while(Vector3.Distance(transform.position, target) > 0.1f)
 				{
 						float step = speed * Time.deltaTime;
 						//transform.position = Vector3.Lerp(transform.position, target, smoothing * Time.deltaTime);
 						transform.position = Vector3.MoveTowards(transform.position, target, step);
 
 						if (target.x < transform.position.x && noSyringe) {
-								currentMainAnimState = CurrentMainCharAnimationState.WalkLeft;
+								//currentMainAnimState = CurrentMainCharAnimationState.WalkLeft;
+								animator.SetInteger ("Direction",1);
 
 						} else if (target.x > transform.position.x && noSyringe) {
-								currentMainAnimState = CurrentMainCharAnimationState.WalkRight;
+								//currentMainAnimState = CurrentMainCharAnimationState.WalkRight;
+								animator.SetInteger ("Direction",2);
 						}
-						else if (target.x < transform.position.x && holdingSyringe) {
-								currentMainAnimState = CurrentMainCharAnimationState.WalkLeftSyringe;
+						/*else if (target.x < transform.position.x && holdingSyringe) {
+								//currentMainAnimState = CurrentMainCharAnimationState.WalkLeftSyringe;
 						}
 						else if (target.x > transform.position.x && holdingSyringe) {
-								currentMainAnimState = CurrentMainCharAnimationState.WalkRightSyringe;
-						}
+								//currentMainAnimState = CurrentMainCharAnimationState.WalkRightSyringe;
+						}*/
+						//animator.SetInteger ("Direction", 0);
 
 						yield return null;
 				}
+
+
+
 		}
 
 		void Update(){
-			switch (currentMainAnimState) {
-			case CurrentMainCharAnimationState.Idle:
-					animator.SetInteger ("Direction",0);
-					break;
-			case CurrentMainCharAnimationState.WalkLeft:
-					animator.SetInteger ("Direction",1);
-					break;
-			case CurrentMainCharAnimationState.WalkRight:
-					animator.SetInteger ("Direction",2);
-					break;
-			case CurrentMainCharAnimationState.WalkLeftSyringe:
-					animator.SetInteger ("Direction",3);
-					break;
-			case CurrentMainCharAnimationState.WalkRightSyringe:
-					animator.SetInteger ("Direction", 4);
-					break;
-			case CurrentMainCharAnimationState.Stab:
-					animator.SetInteger ("Direction", 5);
-					break;
-			case CurrentMainCharAnimationState.Die:
-					animator.SetInteger ("Direction", 6);
-					break;
-			default:
-					break;
-			}
+				if (Vector3.Distance (transform.position, target) < 0.1f) {
+						print ("Should be less than 0.1f away from target");
+						animator.SetInteger ("Direction", 0);
+				}
+
 		}
-
-
-
+				
 		void OnCollisionEnter2D(Collision2D other){
-				//print ("Should be stopping");
+				print ("Should be entering collider and stopping");
 
 				if (other.gameObject.tag == "syringe") {
 						other.gameObject.SetActive (false);
-						//renderer.enabled = false;
-						//stabbyPose.renderer.enabled = true;
-
 						noSyringe = false;
 						holdingSyringe = true; 
 				}
 
+				animator.SetInteger ("Direction", 0);
 				StopCoroutine("Movement");
+
 		}
 
 		void OnCollisionStay2D(Collision2D other){
-				//print ("Should be stopping");
+				print ("Should be staying!");
+				animator.SetInteger ("Direction", 0);
 				StopCoroutine("Movement");
+
 		}
-
-
 }
